f89bd3ac1cbdd1fc670254daa45d7863dd00d7a6
José Miguel Oroño <joseomaker@gmail.com>
2024-02-03 23:25:24 -0500

    Sanitize and implement more Search methods

--- laravel-backend/app/Http/Controllers/SearchController.php
+++ laravel-backend/app/Http/Controllers/SearchController.php
@@ -12,24 +12,61 @@ use Illuminate\Support\Facades\Log;
 
 class SearchController extends Controller
 {
+    
     /**
-     * Buscar pintas usando un query dado y la Spotify API
+     * Buscar pistas usando un query dado y la API de Spotify 
      */
-    public function searchTracks()
-    {   
+    public function searchTracks() {
         $request = request();
         // Get the query from the request url (e.g. /search/tracks?query=hello)
         if ($request->has('query')) {
             $query = $request->input('query');
-            //$results = Spotify::searchTracks($query)->limit(10);
-            $results = Spotify::searchTracks($query)->get();
-            //Log::info('Some message here.');
-            //Log::info($results);
+            $query = htmlspecialchars(strip_tags($query), ENT_QUOTES, 'UTF-8', false);
+            $results = Spotify::searchTracks($query)->limit(10)->get();
             return response()->json($results);
         } else {
             return response()->json([
-                'error' => 'No query provided'
+                'error' => 'No query provided.'
             ]);
         }
     }
+
+    /**
+     * Buscar albums usando un query dado y la API de Spotify 
+     */
+    public function searchAlbums() {   
+        $request = request();
+        // Get the query from the request url (e.g. /search/tracks?query=hello)
+        if ($request->has('query')) {
+            $query = $request->input('query');
+            $query = htmlspecialchars(strip_tags($query), ENT_QUOTES, 'UTF-8', false);
+            $results = Spotify::searchAlbums($query)->limit(10)->get();
+            return response()->json($results);
+        } else {
+            return response()->json([
+                'error' => 'No query provided.'
+            ]);
+        }
+    }
+
+    /**
+     * Buscar artistas usando un query dado y la API de Spotify 
+     */
+    public function searchArtists() {   
+        $request = request();
+        // Get the query from the request url (e.g. /search/tracks?query=hello)
+        if ($request->has('query')) {
+            $query = $request->input('query');
+            $query = htmlspecialchars(strip_tags($query), ENT_QUOTES, 'UTF-8', false);
+            $results = Spotify::searchArtists($query)->limit(10)->get();
+            return response()->json($results);
+        } else {
+            return response()->json([
+                'error' => 'No query provided.'
+            ]);
+        }
+    }
+
+
+
 }
